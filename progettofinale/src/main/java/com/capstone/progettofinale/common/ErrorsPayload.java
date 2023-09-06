3638c0c11652263f3669c9e8451585131eba2ca9
marcodinicola88 <marco.dinicola@yahoo.it>
2023-09-06 14:29:15 +0200

    gestione eccezioni

---
+++ progettofinale/src/main/java/com/capstone/progettofinale/common/ErrorsPayload.java
@@ -0,0 +1,16 @@
+package com.capstone.progettofinale.common;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ErrorsPayload {
+	private String message;
+	private Date timestamp;
+	private int internalCode;
+}
